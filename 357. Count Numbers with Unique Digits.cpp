3093064e3400ac77591adf9e42f321a0bac9a691
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-27 17:40:06 -0500

    Update 357. Count Numbers with Unique Digits.cpp

--- 357. Count Numbers with Unique Digits.cpp
+++ 357. Count Numbers with Unique Digits.cpp
@@ -1,4 +1,26 @@
 //https://www.hrwhisper.me/leetcode-count-numbers-unique-digits/
+//http://www.cnblogs.com/grandyang/p/5582633.html
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            res += count(i);
+        }
+        return res;
+    }
+    int count(int k) {
+        if (k < 1) return 0;
+        if (k == 1) return 10;
+        int res = 1;
+        for (int i = 9; i >= (11 - k); --i) {
+            res *= i;
+        }
+        return res * 9;
+    }
+};
+
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
