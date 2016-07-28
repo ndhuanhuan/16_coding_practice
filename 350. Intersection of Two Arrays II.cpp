1a24d7237b5f74f6cd9abe1bae86a32796a21c4d
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-28 14:33:03 -0500

    Update 350. Intersection of Two Arrays II.cpp

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -21,3 +21,30 @@ public:
         
     }
 };
+
+
+// If the given array is already sorted, and the memory is limited or m ~ n.
+// Time:  O(m + n)
+// Soace: O(1)
+// Two pointers solution.
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        // Make sure it is sorted, doesn't count in time.
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        auto it1 = nums1.cbegin(), it2 = nums2.cbegin();
+        while (it1 != nums1.cend() && it2 != nums2.cend()) {
+            if (*it1 < *it2) {
+                ++it1;
+            } else if (*it1 > *it2) {
+                ++it2;
+            } else {
+                result.emplace_back(*it1);
+                ++it1, ++it2;
+            }
+        }
+        return result;
+    }
+};
