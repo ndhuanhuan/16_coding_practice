058e8f5c76f6c04d5201f5ddcefe30a5853137a1
ndhuanhuan <ndhuanhuan@gmail.com>
2016-07-28 11:14:23 +0800

    Update 355. **Design Twitter.cpp

--- 355. **Design Twitter.cpp
+++ 355. **Design Twitter.cpp
@@ -15,7 +15,7 @@ public:
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     vector<int> getNewsFeed(int userId) {
         vector<int> res;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;     //小顶堆，可以pop时候pop最小的
         for (auto it = friends[userId].begin(); it != friends[userId].end(); ++it) {
             for (auto a = tweets[*it].begin(); a != tweets[*it].end(); ++a) {
                 if (q.size() > 0 && q.top().first > a->first && q.size() > 10) break;
