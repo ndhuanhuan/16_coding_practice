e956c49d54a44c4438a4b187282d03e27ea25086
ndhuanhuan <ndhuanhuan@gmail.com>
2016-08-01 12:06:42 -0500

    Create 341. *Flatten Nested List Iterator.cpp

---
+++ 341. *Flatten Nested List Iterator.cpp
@@ -0,0 +1,29 @@
+//http://www.cnblogs.com/grandyang/p/5358793.html
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; --i) {
+            s.push(nestedList[i]);
+        }
+    }
+
+    int next() {
+        NestedInteger t = s.top(); s.pop();
+        return t.getInteger();
+    }
+
+    bool hasNext() {
+        while (!s.empty()) {
+            NestedInteger t = s.top(); 
+            if (t.isInteger()) return true;
+            s.pop();
+            for (int i = t.getList().size() - 1; i >= 0; --i) {
+                s.push(t.getList()[i]);
+            }
+        }
+        return false;
+    }
+
+private:
+    stack<NestedInteger> s;
+};
