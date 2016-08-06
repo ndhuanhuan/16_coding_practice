cc191a9606332d99cc0cdd377897fd5a02711e27
ndhuanhuan <ndhuanhuan@gmail.com>
2016-08-06 14:18:17 -0500

    Create 315. *Count of Smaller Numbers After Self.cpp

---
+++ 315. *Count of Smaller Numbers After Self.cpp
@@ -0,0 +1,20 @@
+//http://www.cnblogs.com/grandyang/p/5078490.html
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> t,res(nums.size());
+        for(int i=nums.size()-1;i>=0;--i)
+        {
+            int left = 0, right = t.size();
+            while(left<right)
+            {
+                int mid = left+(right-left)/2;
+                if(t[mid]>=nums[i]) right = mid;
+                else left = mid+1;
+            }
+            res[i] = right;
+            t.insert(t.begin()+right,nums[i]);
+        }
+        return res;
+    }
+};
