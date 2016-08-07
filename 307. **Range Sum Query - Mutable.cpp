cb13a556794e6df58c1557fd0203afcf1ca7640a
ndhuanhuan <ndhuanhuan@gmail.com>
2016-08-07 15:14:17 -0500

    Create 307. **Range Sum Query - Mutable.cpp

---
+++ 307. **Range Sum Query - Mutable.cpp
@@ -0,0 +1,34 @@
+//http://www.cnblogs.com/zichi/p/4806998.html
+//http://www.cnblogs.com/grandyang/p/4985506.html
+
+class NumArray {
+public:
+    NumArray(vector<int> &nums) {
+        num.resize(nums.size() + 1);
+        bit.resize(nums.size() + 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            update(i, nums[i]);
+        }
+    }
+    void update(int i, int val) {
+        int diff = val - num[i + 1];
+        for (int j = i + 1; j < num.size(); j += (j&-j)) {
+            bit[j] += diff;
+        }
+        num[i + 1] = val;
+    }
+    int sumRange(int i, int j) {
+        return getSum(j + 1) - getSum(i);
+    }    
+    int getSum(int i) {
+        int res = 0;
+        for (int j = i; j > 0; j -= (j&-j)) {
+            res += bit[j];
+        }
+        return res;
+    }
+
+private:
+    vector<int> num;
+    vector<int> bit;
+};
