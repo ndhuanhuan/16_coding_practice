fab9e9b70dea9cb0cc78613f3fd631c8f6df2bcd
ndhuanhuan <ndhuanhuan@gmail.com>
2016-08-08 12:48:53 -0500

    Create 283. *Move Zeroes.cpp

---
+++ 283. *Move Zeroes.cpp
@@ -0,0 +1,11 @@
+//http://www.cnblogs.com/grandyang/p/4822732.html
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int i = 0, j = 0; i < nums.size(); ++i) {
+            if (nums[i]) {
+                swap(nums[i], nums[j++]);
+            }
+        }
+    }
+};
