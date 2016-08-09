7a5ccfaf125126cf0d81cca56c55c1bea41a5693
ndhuanhuan <ndhuanhuan@gmail.com>
2016-08-09 12:43:14 -0500

    Create 275. **H-Index II.cpp

---
+++ 275. **H-Index II.cpp
@@ -0,0 +1,14 @@
+//http://www.cnblogs.com/grandyang/p/4782695.html
+//https://www.hrwhisper.me/leetcode-h-index-h-index-ii/
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int len = citations.size(), left = 0, right = len;
+        while (left < right) {
+            int mid = 0.5 * (left + right);
+            if (citations[mid] >= len - mid) right = mid;
+            else left = mid + 1;
+        }
+        return len - left;
+    }
+};
