627f86d96aef13aa8f653e14202c1be8dfb49a99
ndhuanhuan <ndhuanhuan@gmail.com>
2016-09-20 15:13:03 -0500

    Update 221. *Maximal Square.cpp

--- 221. *Maximal Square.cpp
+++ 221. *Maximal Square.cpp
@@ -14,7 +14,7 @@ public:
         for (int i = 1; i < M; ++i) {
             for (int j = 1; j < N; ++j) {
                 if (matrix[i][j] == '1') 
-                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                    dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}) + 1;
                 res = max(res, dp[i][j]);
             }
         }
