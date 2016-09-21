77c908b2a154d9b29cf1ab1c155eb54b0b5a8149
ndhuanhuan <ndhuanhuan@gmail.com>
2016-09-21 14:03:57 -0500

    Update 224. **Basic Calculator.cpp

--- 224. **Basic Calculator.cpp
+++ 224. **Basic Calculator.cpp
@@ -38,3 +38,31 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+//http://www.cnblogs.com/grandyang/p/4570699.html
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0;
+        vector<int> sign(2, 1);
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c >= '0') {
+                int num = 0;
+                while (i < s.size() && s[i] >= '0') {
+                    num = 10 * num + s[i++] - '0';
+                }
+                res += sign.back() * num;
+                sign.pop_back();
+                --i;
+            }
+            else if (c == ')') sign.pop_back();
+            else if (c != ' ') sign.push_back(sign.back() * (c == '-' ? -1 : 1));  //makes everything inside () negative or positive 
+        }
+        return res;
+    }
+};
