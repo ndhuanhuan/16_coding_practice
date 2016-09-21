4956716793bf1a8d4ba7afa31b9819cd0e9cd95e
ndhuanhuan <ndhuanhuan@gmail.com>
2016-09-21 12:09:43 +0800

    Create 220. **Contains Duplicate III.cpp

---
+++ 220. **Contains Duplicate III.cpp
@@ -0,0 +1,15 @@
+//http://www.cnblogs.com/grandyang/p/4545261.html
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        map<int, int> m;
+        int j = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i - j > k && m[nums[j]] == j) m.erase(nums[j++]);
+            auto a = m.lower_bound(nums[i] - t);
+            if (a != m.end() && abs(a->first - nums[i]) <= t) return true;
+            m[nums[i]] = i;
+        }
+        return false;
+    }
+}; 
