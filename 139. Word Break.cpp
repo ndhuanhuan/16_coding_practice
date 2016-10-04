c63427f1e7ef00ffb7e1795acf6dd8a1371b014e
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-04 14:51:10 -0500

    Update 139. Word Break.cpp

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -1,1 +1,18 @@
-
+//http://www.cnblogs.com/grandyang/p/4257740.html
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        int len = s.size();
+        vector<bool> res(len + 1, false);
+        res[0] = true;
+        for (int i = 0; i < len + 1; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (res[j] && dict.find(s.substr(j, i-j)) != dict.end()) {
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        return res[len];
+    }
+};
