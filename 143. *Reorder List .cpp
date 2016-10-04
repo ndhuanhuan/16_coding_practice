f82c7ebb0516ca6c6406f183c8bb85c5a37630ce
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-04 14:22:27 -0500

    Update and rename 143. Reorder List .cpp to 143. *Reorder List .cpp

---
+++ 143. *Reorder List .cpp
@@ -0,0 +1,30 @@
+//http://www.cnblogs.com/grandyang/p/4254860.html
+class Solution {
+public:
+    void reorderList(ListNode *head) {
+        if (!head || !head->next || !head->next->next) return;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *mid = slow->next;
+        slow->next = NULL;
+        ListNode *last = mid;
+        ListNode *pre = NULL;
+        while (last) {
+            ListNode *next = last->next;
+            last->next = pre;
+            pre = last;
+            last = next;
+        }
+        while (head && pre) {
+            ListNode *next = head->next;
+            head->next = pre;
+            pre = pre->next;
+            head->next->next = next;
+            head = next;
+        }
+    }
+};
