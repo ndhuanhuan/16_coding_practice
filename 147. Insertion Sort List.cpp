169c2ec6e2abbcc198bbb8ff35e857a440122900
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-04 13:54:48 -0500

    Update 147. Insertion Sort List.cpp

--- 147. Insertion Sort List.cpp
+++ 147. Insertion Sort List.cpp
@@ -1,1 +1,19 @@
 //http://www.cnblogs.com/grandyang/p/4250107.html
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode *res = new ListNode(-1);
+        ListNode *cur = res;
+        while (head) {
+            ListNode *next = head->next;
+            cur = res;
+            while (cur->next && cur->next->val <= head->val) {
+                cur = cur->next;
+            }
+            head->next = cur->next;
+            cur->next = head;
+            head = next;
+        }
+        return res->next;
+    }
+};
