96edb3838d7ced5994828d7e860652ce7069815f
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-06 09:24:05 -0500

    Create 069. Sqrt(x).cpp

---
+++ 069. Sqrt(x).cpp
@@ -0,0 +1,16 @@
+//http://www.cnblogs.com/grandyang/p/4346413.html
+// Binary Search
+class Solution {
+public:
+    int sqrt(int x) {
+        long long left = 0, right = (x / 2) + 1;
+        while (left <= right) {
+            long long mid = (left + right) / 2;
+            long long sq = mid * mid;
+            if (sq == x) return mid;
+            else if (sq < x) left = mid + 1;
+            else right = mid - 1;
+        }
+        return right;
+    }
+};
