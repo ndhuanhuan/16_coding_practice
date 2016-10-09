c36f38db793314a66233012277546a5215a5f05d
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-10 04:43:16 +0800

    Update 157. (To Do)Read N Characters Given Read4.cpp

--- 157. (To Do)Read N Characters Given Read4.cpp
+++ 157. (To Do)Read N Characters Given Read4.cpp
@@ -1,1 +1,13 @@
-
+//http://www.cnblogs.com/grandyang/p/5174322.html
+class Solution {
+public:
+    int read(char *buf, int n) {
+        int res = 0;
+        for (int i = 0; i <= n / 4; ++i) {
+            int cur = read4(buf + res);
+            if (cur == 0) break;
+            res += cur;
+        }
+        return min(res, n);
+    }
+};
