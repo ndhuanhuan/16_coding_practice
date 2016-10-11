725b32e39752c54462e3f5723c3dd51bb8d04afb
ndhuanhuan <ndhuanhuan@gmail.com>
2016-10-11 09:47:44 +0800

    Update 034. Search for a Range.cpp

--- 034. Search for a Range.cpp
+++ 034. Search for a Range.cpp
@@ -21,3 +21,30 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2,-1);
+        const int n = nums.size();
+        int left=0, right = n-1;
+        while(left<right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid]<target) left = mid+1;
+            else right = mid;
+        }
+        if(nums[left]!=target) return res;
+        res[0]=left;
+        right = n-1;  // We don't have to set i to 0 the second time.
+        while (left < right)
+        {
+            int mid = (left + right) /2 + 1;	// Make mid biased to the right
+            if (nums[mid] > target) right = mid - 1;  
+            else left = mid;				// So that this won't make the search range stuck.
+        }
+        res[1]=right;
+        return res;
+    }
+};
