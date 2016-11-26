28abb8728d69905079195fde6a3358e659d544a7
ndhuanhuan <ndhuanhuan@gmail.com>
2016-11-26 16:14:53 -0600

    Update 007. Reverse Integer.cpp

--- 007. Reverse Integer.cpp
+++ 007. Reverse Integer.cpp
@@ -1,13 +1,2 @@
 //http://www.cnblogs.com/grandyang/p/4125588.html
-class Solution {
-public:
-    int reverse(int x) {
-        int res = 0;
-        while (x != 0) {
-            if (abs(res) > INT_MAX / 10) return 0;
-            res = res * 10 + x % 10;
-            x /= 10;
-        }
-        return res;
-    }
-};
+ 
