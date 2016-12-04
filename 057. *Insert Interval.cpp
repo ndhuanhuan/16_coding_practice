e6ca0a68c44a7ad2aa732e2de83c15b0511299a7
ndhuanhuan <ndhuanhuan@gmail.com>
2016-12-04 16:21:23 -0600

    Update 057. *Insert Interval.cpp

--- 057. *Insert Interval.cpp
+++ 057. *Insert Interval.cpp
@@ -1,4 +1,15 @@
 //http://www.cnblogs.com/grandyang/p/4367569.html
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
 class Solution {
 public:
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
