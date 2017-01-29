deb5c6c9b55a114cb7ea066b3a1b1c8812e997b3
ndhuanhuan <ndhuanhuan@gmail.com>
2017-01-28 23:36:32 -0600

    Update 140. **Word Break II.cpp

--- 140. **Word Break II.cpp
+++ 140. **Word Break II.cpp
@@ -25,3 +25,35 @@ public:
         }
     }
 };
+
+
+
+class Solution {
+    unordered_map<string, vector<string>> m;
+
+    vector<string> combine(string word, vector<string> prev){
+        for(int i=0;i<prev.size();++i){
+            prev[i]+=" "+word;
+        }
+        return prev;
+    }
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        if(m.count(s)) return m[s]; //take from memory
+        vector<string> result;
+        if(find(wordDict.begin(),wordDict.end(),s)!=wordDict.end()){ //a whole string is a word
+            result.push_back(s);
+        }
+        for(int i=1;i<s.size();++i){
+            string word=s.substr(i);
+            if(find(wordDict.begin(),wordDict.end(),word)!=wordDict.end()){
+                string rem=s.substr(0,i);
+                vector<string> prev=combine(word,wordBreak(rem,wordDict));
+                result.insert(result.end(),prev.begin(), prev.end());
+            }
+        }
+        m[s]=result; //memorize
+        return result;
+    }
+};
