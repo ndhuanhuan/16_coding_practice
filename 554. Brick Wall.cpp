08c3b9d4a46bfcb2f9c835911af808347bfbb28c
ndhuanhuan <ndhuanhuan@gmail.com>
2018-02-11 20:25:32 -0600

    Update 554. Brick Wall.cpp

--- 554. Brick Wall.cpp
+++ 554. Brick Wall.cpp
@@ -15,3 +15,21 @@ public:
         return wall.size() - maxend;
     }
 };
+
+//http://www.cnblogs.com/grandyang/p/6697067.html
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        int mx = 0;
+        unordered_map<int, int> m;
+        for (auto a : wall) {
+            int sum = 0;
+            for (int i = 0; i < a.size() - 1; ++i) {
+                sum += a[i];
+                ++m[sum];
+                mx = max(mx, m[sum]);
+            }
+        }
+        return wall.size() - mx;
+    }
+};
