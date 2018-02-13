e318554b7f1d690c0ba441df4c4318151c968ae2
ndhuanhuan <ndhuanhuan@gmail.com>
2018-02-13 01:29:47 -0600

    Update 477. Total Hamming Distance.cpp

--- 477. Total Hamming Distance.cpp
+++ 477. Total Hamming Distance.cpp
@@ -1,3 +1,7 @@
+//http://www.cnblogs.com/grandyang/p/6208062.html
+//我们仔细观察累计汉明距离和0跟1的个数，
+//我们可以发现其实就是0的个数乘以1的个数，
+//发现了这个重要的规律，那么整道题就迎刃而解了，只要统计出每一位的1的个数即可
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
