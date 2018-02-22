4e8ba8f6736f2e492f6a4db1dfa3ab1728a682d2
ndhuanhuan <ndhuanhuan@gmail.com>
2018-02-21 23:08:07 -0600

    Update 274. *H-Index.cpp

--- 274. *H-Index.cpp
+++ 274. *H-Index.cpp
@@ -10,3 +10,28 @@ public:
 		return 0;
 	}
 };
+
+
+//Better
+//https://segmentfault.com/a/1190000003794831
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int N = citations.size();
+        vector<int> count(N+1,0);
+        for (int n : citations)
+        {
+            if (n > N)
+                n = N;
+            count[n]++;
+        }
+        int sum = 0;
+        for (int i = N; i >= 0; i--)
+        {
+            sum += count[i];
+            if (sum >= i)
+                return i;
+        }
+        return 0;
+    }
+};
