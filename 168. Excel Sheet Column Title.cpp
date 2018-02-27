437481adf9b1fb61469767e2e83e3412e29aeccb
ndhuanhuan <ndhuanhuan@gmail.com>
2018-02-27 01:40:09 -0600

    Update 168. Excel Sheet Column Title.cpp

--- 168. Excel Sheet Column Title.cpp
+++ 168. Excel Sheet Column Title.cpp
@@ -2,19 +2,11 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-        string res = "";
+        string res;
         while (n) {
-            if (n % 26 == 0) {
-                res += 'Z';
-                n -= 26;
-            }
-            else {
-                res += n%26 - 1 + 'A';
-                n -= n%26;
-            }
+            res += --n % 26 + 'A';
             n /= 26;
         }
-        reverse(res.begin(), res.end());
-        return res;
+        return string(res.rbegin(), res.rend());
     }
 };
