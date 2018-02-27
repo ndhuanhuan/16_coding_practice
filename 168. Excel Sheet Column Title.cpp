19faaec90de096b6ec4e098e1e059896d4b66fb2
ndhuanhuan <ndhuanhuan@gmail.com>
2018-02-27 01:22:51 -0600

    Create 168. Excel Sheet Column Title.cpp

---
+++ 168. Excel Sheet Column Title.cpp
@@ -0,0 +1,20 @@
+//http://www.cnblogs.com/grandyang/p/4227618.html
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res = "";
+        while (n) {
+            if (n % 26 == 0) {
+                res += 'Z';
+                n -= 26;
+            }
+            else {
+                res += n%26 - 1 + 'A';
+                n -= n%26;
+            }
+            n /= 26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
