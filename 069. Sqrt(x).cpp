c3c7612d330ad22a4891062f354d97abaf010101
ndhuanhuan <ndhuanhuan@gmail.com>
2018-03-06 00:19:03 -0600

    Update 069. Sqrt(x).cpp

--- 069. Sqrt(x).cpp
+++ 069. Sqrt(x).cpp
@@ -15,6 +15,20 @@ public:
     }
 };
 
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x <= 1) return x;
+        int left = 0, right = x;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (x / mid >= mid) left = mid + 1;
+            else right = mid;
+        }
+        return right - 1;
+    }
+};
+
 //Newton
 class Solution {
 public:
