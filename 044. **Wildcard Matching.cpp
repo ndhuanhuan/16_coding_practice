a58019799f8a66b2c75861c385608a394fba001b
ndhuanhuan <ndhuanhuan@gmail.com>
2018-03-08 00:26:38 -0600

    Update 044. **Wildcard Matching.cpp

--- 044. **Wildcard Matching.cpp
+++ 044. **Wildcard Matching.cpp
@@ -20,3 +20,40 @@ public:
         return dp[m][n];
     }
 };
+
+
+
+
+//https://leetcode.com/problems/wildcard-matching/discuss/17811/My-three-C++-solutions-(iterative-(16ms)-and-DP-(180ms)-and-modified-recursion-(88ms))
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(s.c_str(), p.c_str());
+    }
+    
+     bool isMatch(const char *s, const char *p) {
+        const char* star=NULL;
+        const char* ss=s;
+        while (*s){
+            //advancing both pointers when (both characters match) or ('?' found in pattern)
+            //note that *p will not advance beyond its length 
+            if ((*p=='?')||(*p==*s)){s++;p++;continue;} 
+
+            // * found in pattern, track index of *, only advancing pattern pointer 
+            if (*p=='*'){star=p++; ss=s;continue;} 
+
+            //current characters didn't match, last pattern pointer was *, current pattern pointer is not *
+            //only advancing pattern pointer
+            if (star){ p = star+1; s=++ss;continue;} 
+
+           //current pattern pointer is not star, last patter pointer was not *
+           //characters do not match
+            return false;
+        }
+
+       //check for remaining characters in pattern
+        while (*p=='*'){p++;}
+
+        return !*p;  
+    }
+};
