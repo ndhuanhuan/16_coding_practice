8e2c9c6dade0495e75300b2e0d5f597737101f8b
ndhuanhuan <ndhuanhuan@gmail.com>
2018-03-16 00:07:04 -0500

    Create 285. Inorder Successor in BST.cpp

---
+++ 285. Inorder Successor in BST.cpp
@@ -0,0 +1,20 @@
+//http://www.cnblogs.com/grandyang/p/5306162.html
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        stack<TreeNode*> s;
+        bool b = false;
+        TreeNode *t = root;
+        while (t || !s.empty()) {
+            while (t) {
+                s.push(t);
+                t = t->left;
+            }
+            t = s.top(); s.pop();
+            if (b) return t;
+            if (t == p) b = true;
+            t = t->right;
+        }
+        return NULL;
+    }
+};
