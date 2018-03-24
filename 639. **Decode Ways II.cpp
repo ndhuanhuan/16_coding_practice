60d1c30c9de1e66841b879ee37af68ebc814c594
ndhuanhuan <ndhuanhuan@gmail.com>
2018-03-24 17:11:17 -0500

    Update 639. **Decode Ways II.cpp

--- 639. **Decode Ways II.cpp
+++ 639. **Decode Ways II.cpp
@@ -1,3 +1,90 @@
+//http://zxi.mytechroad.com/blog/dynamic-programming/leetcode-639-decode-ways-ii/
+
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+// Author: Huahua
+// Runtime: 58 ms
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty()) return 0;        
+        //           dp[-1]  dp[0]
+        long dp[2] = {1, ways(s[0])};
+        for (int i = 1; i < s.length(); ++i) {
+            long dp_i = ways(s[i]) * dp[1] + ways(s[i - 1], s[i]) * dp[0];
+            dp_i %= kMod;
+            dp[0] = dp[1];
+            dp[1] = dp_i;
+        }
+        return dp[1];
+    }
+private:
+    static constexpr int kMod = 1000000007;    
+    
+    int ways(char c) {
+        if (c == '0') return 0;
+        if (c == '*') return 9;
+        return 1;
+    }
+    
+    int ways(char c1, char c2) {
+        if (c1 == '*' && c2 == '*') 
+            return 15;
+        if (c1 == '*') {
+          return (c2 >= '0' && c2 <= '6') ? 2 : 1;
+        } else if (c2 == '*') {
+            switch (c1) {
+                case '1': return 9;
+                case '2': return 6;
+                default: return 0;
+            }
+        } else {
+            int prefix = (c1 - '0') * 10 + (c2 - '0');
+            return prefix >= 10 && prefix <= 26;
+        }        
+    }
+};
+
+
+
+
 //https://leetcode.com/problems/decode-ways-ii/discuss/105265/
 class Solution {
 public:
@@ -33,3 +120,4 @@ private:
             return stoi(s) >= 10 && stoi(s) <= 26? 1:0; 
     }
 };
+
