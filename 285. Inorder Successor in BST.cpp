49ae3b1cb0eced9b35b57f5fac79ea235b103a40
ndhuanhuan <ndhuanhuan@gmail.com>
2018-03-30 01:14:13 -0500

    Update 285. Inorder Successor in BST.cpp

--- 285. Inorder Successor in BST.cpp
+++ 285. Inorder Successor in BST.cpp
@@ -2,6 +2,22 @@
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        TreeNode *res = NULL;
+        while (root) {
+            if (root->val > p->val) {
+                res = root;
+                root = root->left;
+            } else root = root->right;
+        }
+        return res;
+    }
+};
+
+
+
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
         stack<TreeNode*> s;
         bool b = false;
         TreeNode *t = root;
