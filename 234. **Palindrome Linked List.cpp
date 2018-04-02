0a8a0aab98e5d2472cda90842cda5f67e8e9789d
ndhuanhuan <ndhuanhuan@gmail.com>
2018-04-01 20:16:23 -0500

    Update 234. **Palindrome Linked List.cpp

--- 234. **Palindrome Linked List.cpp
+++ 234. **Palindrome Linked List.cpp
@@ -1,3 +1,46 @@
+//https://www.jiuzhang.com/solution/palindrome-linked-list/#tag-highlight-lang-cpp
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == NULL || head ->next == NULL) {
+            return true;
+        }
+        ListNode* mid = findmid(head);
+        mid->next = reverse(mid->next);
+        mid = mid->next;
+        while(head!=NULL && mid!=NULL) {
+            if(head->val != mid->val) {
+                return false;
+            }
+            head = head->next;
+            mid = mid->next;
+        }
+        return true;
+    }
+    ListNode * findmid(ListNode * now) {
+        ListNode* slow = now;
+        ListNode* fast = now ->next;
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode * reverse(ListNode *now) {
+        ListNode * pre = NULL;
+        while(now!=NULL) {
+            ListNode *temp = now->next;
+            now->next = pre;
+            pre = now;
+            now = temp;
+        }
+        return pre;
+    }
+};
+
+
+
+
 //http://www.cnblogs.com/grandyang/p/4635425.html
 class Solution {
 public:
