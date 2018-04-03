035e3400f8bdfc3d3e4124ab934de6bd8f6bf84b
ndhuanhuan <ndhuanhuan@gmail.com>
2018-04-02 23:51:03 -0500

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,3 +1,21 @@
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		if (head == NULL || head->next == NULL) return head;
+
+		ListNode* pre = NULL;
+        while(head != NULL) {
+            ListNode* tmp = head->next;
+            head->next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+	}
+};
+
+
+
 //http://www.cnblogs.com/grandyang/p/4478820.html
 class Solution {
 public:
