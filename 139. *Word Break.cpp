af0cc8fbe39a93d042803d8dd69088ff01c4f39c
ndhuanhuan <ndhuanhuan@gmail.com>
2018-04-06 00:14:44 -0500

    Update 139. *Word Break.cpp

--- 139. *Word Break.cpp
+++ 139. *Word Break.cpp
@@ -17,6 +17,29 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict;
+        for(string& s: wordDict)
+        {
+            dict.insert(s);
+        }
+        int len = s.size();
+        vector<bool> res(len+1, false);
+        res[0] = true;
+        for(int i=0; i< len+1; ++i) {
+            for(int j=0; j< i; ++j) {
+                if(res[j] && dict.find(s.substr(j, i-j)) != dict.end()) {
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        return res[len];
+    }
+};
+
 
 class Solution {
 public:
