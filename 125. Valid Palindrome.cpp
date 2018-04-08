c78a29005d1ef3ffe211e051f972f75f476e1ccb
ndhuanhuan <ndhuanhuan@gmail.com>
2018-04-08 17:48:06 -0500

    Update 125. Valid Palindrome.cpp

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -4,6 +4,26 @@ public:
     bool isPalindrome(string s) {
         int left = 0, right = s.size() - 1 ;
         while (left < right) {
+            if (!isalnum(s[left])) ++left;
+            else if (!isalnum(s[right])) --right;
+            else if ((s[left] + 32 - 'a') %32 != (s[right] + 32 - 'a') % 32) return false;
+            else {
+                ++left; --right;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1 ;
+        while (left < right) {
             if (!isAlphaNum(s[left])) ++left;
             else if (!isAlphaNum(s[right])) --right;
             else if ((s[left] + 32 - 'a') %32 != (s[right] + 32 - 'a') % 32) return false;
