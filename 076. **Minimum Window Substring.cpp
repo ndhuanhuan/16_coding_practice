a51f8b402e3ec5443409168f44d19646ba147b87
ndhuanhuan <ndhuanhuan@gmail.com>
2018-04-10 01:11:38 -0500

    Update 076. **Minimum Window Substring.cpp

--- 076. **Minimum Window Substring.cpp
+++ 076. **Minimum Window Substring.cpp
@@ -1,3 +1,27 @@
+
+https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems?page=2
+
+
+
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+    }
+};
+
+
 //http://www.cnblogs.com/grandyang/p/4340948.html
 class Solution {
 public:
