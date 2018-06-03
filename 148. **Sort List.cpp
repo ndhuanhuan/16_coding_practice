c30afb329bad86111b085bd48f8738d206f7391b
ndhuanhuan <ndhuanhuan@gmail.com>
2018-06-03 18:52:30 -0500

    Update 148. **Sort List.cpp

--- 148. **Sort List.cpp
+++ 148. **Sort List.cpp
@@ -1,4 +1,35 @@
 //http://www.cnblogs.com/grandyang/p/4249905.html
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *slow = head, *fast = head, *pre = head;
+        while (fast && fast->next) {
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        pre->next = NULL;
+        return merge(sortList(head), sortList(slow));
+    }
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
+
+
+
+
+
 //merge sort
 class Solution {
 public:
