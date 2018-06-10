9a46862c229f9bbe6dbe6d437d3d2372bbb59d6c
ndhuanhuan <ndhuanhuan@gmail.com>
2018-06-10 13:44:57 -0500

    Update 245. Shortest Word Distance III.cpp

--- 245. Shortest Word Distance III.cpp
+++ 245. Shortest Word Distance III.cpp
@@ -2,6 +2,19 @@
 class Solution {
 public:
     int shortestWordDistance(vector<string>& words, string word1, string word2) {
+        int p1 = words.size(), p2 = -words.size(), res = INT_MAX;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i] == word1) p1 = word1 == word2 ? p2 : i;
+            if (words[i] == word2) p2 = i;
+            res = min(res, abs(p1 - p2));
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int shortestWordDistance(vector<string>& words, string word1, string word2) {
         int p1 = -1, p2 = -1, res = INT_MAX;
         for (int i = 0; i < words.size(); ++i) {
             int t = p1;
