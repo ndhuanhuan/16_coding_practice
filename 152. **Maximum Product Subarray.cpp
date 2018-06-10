3447d521a3e5738c5121796e120eba7c28593042
ndhuanhuan <ndhuanhuan@gmail.com>
2018-06-10 00:37:04 -0500

    Update 152. **Maximum Product Subarray.cpp

--- 152. **Maximum Product Subarray.cpp
+++ 152. **Maximum Product Subarray.cpp
@@ -2,6 +2,23 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        int res = nums[0], n = nums.size();
+        vector<int> f(n, 0), g(n, 0);
+        f[0] = nums[0];
+        g[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            f[i] = max(max(f[i - 1] * nums[i], g[i - 1] * nums[i]), nums[i]);
+            g[i] = min(min(f[i - 1] * nums[i], g[i - 1] * nums[i]), nums[i]);
+            res = max(res, f[i]);
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
         if (nums.empty()) return 0;
         int res = nums[0], mn = nums[0], mx = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
