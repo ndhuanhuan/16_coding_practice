e3d500e629dd39906c75580c662a30a4c4c57049
ndhuanhuan <ndhuanhuan@gmail.com>
2018-06-12 22:59:12 -0500

    Update 464. **Can I Win.cpp

--- 464. **Can I Win.cpp
+++ 464. **Can I Win.cpp
@@ -1,3 +1,31 @@
+//http://zxi.mytechroad.com/blog/searching/leetcode-464-can-i-win/
+
+class Solution {
+public:
+  bool canIWin(int M, int T) {
+    const int sum = M * (M + 1) / 2;
+    if (sum < T) return false;
+    if (T <= 0) return true;
+    m_ = vector<char>(1 << M, 0);
+    return canIWin(M, T, 0);
+  }
+private:
+  vector<char> m_; // 0: unknown, 1: won, -1: lost
+  bool canIWin(int M, int T, int state) {
+    if (T <= 0) return false;
+    if (m_[state]) return m_[state] == 1;
+    for (int i = 0; i < M; ++i) {
+      if (state & (1 << i)) continue; // number i used      
+      // The next player can not win, current player wins
+      if (!canIWin(M, T - (i + 1), state | (1 << i))) 
+        return m_[state] = 1;
+    }
+    // Current player loses.
+    m_[state] = -1;
+    return false;
+  }
+};
+
 //http://www.cnblogs.com/grandyang/p/6103525.html
 class Solution {
 public:
