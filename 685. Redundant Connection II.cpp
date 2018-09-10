6c396c8465f40ae7fa7e017ead1d06a77f7ea31d
ndhuanhuan <ndhuanhuan@gmail.com>
2018-09-09 22:03:11 -0500

    Update 685. Redundant Connection II.cpp

--- 685. Redundant Connection II.cpp
+++ 685. Redundant Connection II.cpp
@@ -4,12 +4,12 @@ class Solution {
 public:
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
         
-        vector<int> parents(edges.size() + 1, 0);        
+        vector<int> parents(edges.size() + 1, 0);
+        vector<int> roots(edges.size() + 1, 0);       
+        vector<int> sizes(edges.size() + 1, 1);
         
         vector<int> ans1;
-        vector<int> ans2;        
-        
-        bool dup_parents = false;
+        vector<int> ans2;
         
         for(auto& edge: edges) {
             int u = edge[0];
@@ -19,16 +19,13 @@ public:
             if (parents[v] > 0) {
                 ans1 = {parents[v], v};
                 ans2 = edge;
-                dup_parents = true;
+                
                 // Delete the later edge
                 edge[0] = edge[1] = -1;
-            } else {            
-                parents[v] = u;
             }
+            
+            parents[v] = u;
         }
-        
-        // Reset parents
-        parents = vector<int>(edges.size() + 1, 0);
         
         for(const auto& edge: edges) {
             int u = edge[0];
@@ -37,21 +34,32 @@ public:
             // Invalid edge (we deleted in step 1)
             if (u < 0 || v < 0) continue;
             
-            parents[v] = u;
+            if (!roots[u]) roots[u] = u;
+            if (!roots[v]) roots[v] = v;
+            int pu = find(u, roots);
+            int pv = find(v, roots);
             
-            if (cycle(v, parents))
-                return dup_parents ? ans1 : edge;
+            // Both u and v are already in the tree
+            if (pu == pv)
+                return ans1.empty() ? edge : ans1;
+            
+            // Unoin, always merge smaller set (pv) to larger set (pu)
+            if (sizes[pv] > sizes[pu])
+                swap(pu, pv);
+            
+            roots[pv] = pu;
+            sizes[pu] += sizes[pv];
         }
         
         return ans2;
-    }    
+    }
+    
 private:
-    bool cycle(int v, const vector<int>& parents) {
-        int u = parents[v];        
-        while (u) {
-            if (u == v) return true;            
-            u = parents[u];
+    int find(int node, vector<int>& roots) {
+        while (roots[node] != node) {
+            roots[node] = roots[roots[node]];
+            node = roots[node];
         }
-        return false;
+        return node;
     }
 };
