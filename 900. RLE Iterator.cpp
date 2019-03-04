3350523be22587f8eb4df571a957bb4fd8d2362b
ndhuanhuan <ndhuanhuan@gmail.com>
2019-03-03 23:37:19 -0600

    Create 900. RLE Iterator.cpp

---
+++ 900. RLE Iterator.cpp
@@ -0,0 +1,22 @@
+// https://zxi.mytechroad.com/blog/simulation/leetcode-900-rle-iterator/
+class RLEIterator {
+public:
+  RLEIterator(vector<int> A): A_(std::move(A)), i_(0) {}
+ 
+  int next(int n) {
+    while (n && i_ < A_.size()) {
+      if (n >= A_[i_]) {
+        n -= A_[i_];
+        i_ += 2;
+        if (n == 0) return A_[i_ - 1];
+      } else {
+        A_[i_] -= n;
+        return A_[i_ + 1];
+      }        
+    }
+    return -1;
+  }
+private:
+  int i_;
+  vector<int> A_;
+};
