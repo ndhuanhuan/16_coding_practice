9c5bdf4c15fdbed32b143307cb81b6b5f8621f7e
ndhuanhuan <ndhuanhuan@gmail.com>
2019-03-21 01:28:27 -0500

    Create 903. **Valid Permutations for DI Sequence.cpp

---
+++ 903. **Valid Permutations for DI Sequence.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-permutations-for-di-sequence/discuss/196939/Easy-to-understand-solution-with-detailed-explanation
+class Solution {
+public:
+    int numPermsDISequence(string S) {
+        int n = S.size(), m = 1e9 + 7;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++)
+            for(int j = 0; j <= i; j++)
+                if(S[i-1] == 'D')
+                    for(int k = j; k <= i-1; k++)
+                        dp[i][j] = dp[i][j]%m + dp[i-1][k]%m;
+                else
+                    for(int k = 0; k <= j-1; k++)
+                        dp[i][j] = dp[i][j]%m + dp[i-1][k]%m;
+        int res = 0;
+        for(int i = 0; i <= n; i++)
+            res = res%m + dp[n][i]%m;
+        return res%m;
+    }
+};
