47e6ad68fc52c0f8168901c5095b9b7f985c55f1
ndhuanhuan <ndhuanhuan@gmail.com>
2019-04-07 15:13:13 -0500

    Update 891. **Super Egg Drop.cpp

--- 891. **Super Egg Drop.cpp
+++ 891. **Super Egg Drop.cpp
@@ -35,3 +35,21 @@ private:
     return m_[k][n] = 1 + dp(k - 1, l - 1);
   }
 };
+
+
+
+
+// https://leetcode.com/problems/super-egg-drop/discuss/158974/C%2B%2BJavaPython-2D-and-1D-DP-O(KlogN)
+class Solution {
+public:
+    int superEggDrop(int K, int N) {
+        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+        int m = 0;
+        while (dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+        }
+        return m;
+    }
+};
