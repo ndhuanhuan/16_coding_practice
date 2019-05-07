9d7ec0c88e6c054f0fa1f228385cb1bd1b6dd742
ndhuanhuan <ndhuanhuan@gmail.com>
2019-05-07 01:42:15 -0500

    Create 862. **Shortest Subarray with Sum at Least K.cpp

---
+++ 862. **Shortest Subarray with Sum at Least K.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/143726/C%2B%2BJavaPython-O(N)-Using-Deque
+// https://buptwc.com/2018/07/02/Leetcode-862-Shortest-Subarray-with-Sum-at-Least-K/
+class Solution {
+public:
+    int shortestSubarray(vector<int> A, int K) {
+        int N = A.size(), res = N + 1;
+        vector<int> B(N + 1, 0);
+        for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
+        deque<int> d;
+        for (int i = 0; i < N + 1; i++) {
+            while (d.size() > 0 && B[i] - B[d.front()] >= K)
+                res = min(res, i - d.front()), d.pop_front();
+            while (d.size() > 0 && B[i] <= B[d.back()]) d.pop_back();
+            d.push_back(i);
+        }
+        return res <= N ? res : -1;
+    }
+};
