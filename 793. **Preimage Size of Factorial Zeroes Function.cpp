fd6577fabf4e0ab8c354f3627b808adcb297e82b
ndhuanhuan <ndhuanhuan@gmail.com>
2019-06-09 17:16:10 -0500

    Update 793. **Preimage Size of Factorial Zeroes Function.cpp

--- 793. **Preimage Size of Factorial Zeroes Function.cpp
+++ 793. **Preimage Size of Factorial Zeroes Function.cpp
@@ -1,4 +1,31 @@
 //http://zxi.mytechroad.com/blog/math/leetcode-793-preimage-size-of-factorial-zeroes-function/
+// https://www.cnblogs.com/grandyang/p/9214055.html
+
+class Solution {
+public:
+    int preimageSizeFZF(int K) {
+        long left = 0, right = 5L * (K + 1);
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            long cnt = numOfTrailingZeros(mid);
+            if (cnt == K) return 5;
+            else if (cnt < K) left = mid + 1;
+            else right = mid;
+        }
+        return 0;
+    }
+    long numOfTrailingZeros(long x) {
+        long res = 0;
+        for (; x > 0; x /= 5) {
+            res += x / 5;
+        }
+        return res;
+    }
+};
+
+
+
+
 class Solution {
 public:
   int preimageSizeFZF(int K) {
