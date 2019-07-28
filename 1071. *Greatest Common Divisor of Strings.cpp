6aba29e6d743b12f85704aed735839633ec5be44
ndhuanhuan <ndhuanhuan@gmail.com>
2019-07-28 01:56:34 -0500

    Create 1071. *Greatest Common Divisor of Strings.cpp

---
+++ 1071. *Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/discuss/303759/My-4-Lines-C%2B%2B-Solution
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1.size() < str2.size()) return gcdOfStrings(str2, str1);
+        if (str2.empty()) return str1;
+        if (str1.substr(0, str2.size()) != str2) return "";
+        return gcdOfStrings(str1.substr(str2.size()), str2);
+    }
+};
