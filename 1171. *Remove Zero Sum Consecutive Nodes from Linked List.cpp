50c8d71b8a304c7bbdec5055c2e830bc9e51f082
ndhuanhuan <ndhuanhuan@gmail.com>
2019-09-19 00:57:23 -0500

    Update 1171. *Remove Zero Sum Consecutive Nodes from Linked List.cpp

--- 1171. *Remove Zero Sum Consecutive Nodes from Linked List.cpp
+++ 1171. *Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -2,6 +2,34 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode dummy(0), *cur = &dummy;
+        dummy.next = head;
+        int prefix = 0;
+        unordered_map<int, ListNode*> m;
+        while (cur) {
+            prefix += cur->val;
+            if (m.count(prefix)) {
+                cur =  m[prefix]->next;
+                int p = prefix + cur->val;
+                while (p != prefix) {
+                    m.erase(p);
+                    cur = cur->next;
+                    p += cur->val;
+                }
+                m[prefix]->next = cur->next;
+            } else {
+                m[prefix] = cur;
+            }
+            cur = cur->next;
+        }
+        return dummy.next;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
         
         ListNode* root =new ListNode(0);
         root->next=head;
