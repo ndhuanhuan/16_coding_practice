784a39a7fda245983c1c7b8f041af3d6ad79ec41
ndhuanhuan <ndhuanhuan@gmail.com>
2019-10-20 19:03:11 -0500

    Update and rename 1227. Airplane Seat Assignment Probability.cpp to 1227. *Airplane Seat Assignment Probability.cpp

--- 1227. *Airplane Seat Assignment Probability.cpp
+++ 1227. *Airplane Seat Assignment Probability.cpp
@@ -1,4 +1,5 @@
 // https://medium.com/i-math/solving-an-advanced-probability-problem-with-virtually-no-math-5750707885f1
+// https://leetcode.com/problems/airplane-seat-assignment-probability/discuss/407533/Python-from-O(n)-to-O(1)-with-detailed-explanation
 class Solution 
 {
     public:
