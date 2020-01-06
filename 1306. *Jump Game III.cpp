90dc3f606f1d4f86e926f43e00575d7842baf2a8
ndhuanhuan <ndhuanhuan@gmail.com>
2020-01-06 01:15:22 -0600

    Update 1306. *Jump Game III.cpp

--- 1306. *Jump Game III.cpp
+++ 1306. *Jump Game III.cpp
@@ -10,3 +10,16 @@ public:
         return false;
     }
 };
+
+// https://leetcode.com/problems/jump-game-iii/discuss/469318/C%2B%2B-Recursion-using-DFS-O(n)
+class Solution {
+public:
+    set<int> se;   
+    bool canReach(vector<int>& arr, int st) {
+        if (st >= 0 && st < arr.size() && se.find(st) == se.end()) {
+            se.insert(st);
+            return arr[st] == 0 || canReach(arr, st + arr[st]) || canReach(arr, st - arr[st]);
+        }
+        return false;       
+    }
+};
