6dc21f61466a902fb834f4ac2f22cfb590d24e6e
ndhuanhuan <ndhuanhuan@gmail.com>
2020-01-30 00:51:51 -0600

    Create 1316. ?Distinct Echo Substrings.cpp

---
+++ 1316. ?Distinct Echo Substrings.cpp
@@ -0,0 +1,15 @@
+// https://zxi.mytechroad.com/blog/string/leetcode-1316-distinct-echo-substrings/
+// TODO: rolling hash solution
+class Solution {
+public:
+  int distinctEchoSubstrings(string text) {
+    const int n = text.length();
+    string_view t(text);
+    unordered_set<string_view> s;    
+    for (int k = 1; k <= n / 2; ++k)
+      for (int i = 0; i + k <= n; ++i)
+        if (t.substr(i, k) == t.substr(i + k, k))
+          s.insert(t.substr(i, 2 * k));
+    return s.size();
+  }
+};
