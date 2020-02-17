0bdf327e62091c0c6e25258fe3344dafd6d3e608
ndhuanhuan <ndhuanhuan@gmail.com>
2020-02-17 17:42:31 -0600

    Update and rename 005.(MS)**Longest Palindromic Substring.cpp to 005.**Longest Palindromic Substring.cpp

---
+++ 005.**Longest Palindromic Substring.cpp
@@ -0,0 +1,26 @@
+// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-5-longest-palindromic-substring/
+class Solution {
+public:
+  string longestPalindrome(string s) {
+    const int n = s.length();
+    auto getLen = [&](int l, int r) {
+      while (l >= 0 && r < n 
+             && s[l] == s[r]) {
+        --l;
+        ++r;
+      }
+      return r - l - 1;
+    };
+    int len = 0;
+    int start = 0;
+    for (int i = 0; i < n; ++i) {
+      int cur = max(getLen(i, i), 
+                    getLen(i, i + 1));
+      if (cur > len) {
+        len = cur;
+        start = i - (len - 1) / 2;
+      }
+    }
+    return s.substr(start, len);
+  }
+};
