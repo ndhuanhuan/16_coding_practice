835823602a08cff5e6c3c8fa5bc7999a06f39299
ndhuanhuan <ndhuanhuan@gmail.com>
2020-02-25 01:03:24 -0600

    Update and rename 1358. Number of Substrings Containing All Three Characters.cpp to 1358. *Number of Substrings Containing All Three Characters.cpp

--- 1358. *Number of Substrings Containing All Three Characters.cpp
+++ 1358. *Number of Substrings Containing All Three Characters.cpp
@@ -1,4 +1,8 @@
 // https://zxi.mytechroad.com/blog/string/1358-number-of-substrings-containing-all-three-characters/
+// "abcabc"
+// when i = 3, 1 + min_element({0,1,2}) => 1, which is "abc"
+// when i = 4, 1 + min_element({3,1,2}) => 2, which is "abca", "bca"
+// ...
 class Solution {
 public:
   int numberOfSubstrings(string s) {
