205e1d2f102692e820603da5de7b0aba0739a0b4
ndhuanhuan <ndhuanhuan@gmail.com>
2020-05-10 18:41:04 -0500

    Update 967. Numbers With Same Consecutive Differences.cpp

--- 801-1000/967. Numbers With Same Consecutive Differences.cpp
+++ 801-1000/967. Numbers With Same Consecutive Differences.cpp
@@ -10,14 +10,14 @@ public:
   }
 private:
   void dfs(int N, int K, int cur, vector<int>& ans) {
-    if (N == 0) {
+    if (N == 0) {     // because of this logic, when trigger dfs in numsSameConsecDiff function, it uses N-1 instead of N
       ans.push_back(cur);
       return;
     }
     int l = cur % 10;
     if (l + K < 10)
       dfs(N - 1, K, cur * 10 + l + K, ans);
-    if (l >= K && K != 0)
+    if (l >= K && K != 0)  // tricky, K!=0, otherwise there will be duplicate results
       dfs(N - 1, K, cur * 10 + l - K, ans);
   }
 };
