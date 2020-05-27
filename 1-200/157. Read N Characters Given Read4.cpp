c285f03412f94292eb55b32c70fba15e1de81775
ndhuanhuan <ndhuanhuan@gmail.com>
2020-05-27 00:08:20 -0500

    Update 157. Read N Characters Given Read4.cpp

--- 1-200/157. Read N Characters Given Read4.cpp
+++ 1-200/157. Read N Characters Given Read4.cpp
@@ -1,4 +1,27 @@
 // Use this: https://www.acwing.com/solution/LeetCode/content/253/
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int s = 0;
+        char *p = buf, temp[4];
+        while(s < n) {
+            int t = read4(temp);
+            for(int i=0; i < t && s < n; ++i) {
+                *p++ = temp[i];
+                s++;
+            }
+            if(t < 4) return s;
+        }
+        return s;
+    }
+};
+
+
 //http://www.cnblogs.com/grandyang/p/5174322.html
 class Solution {
 public:
