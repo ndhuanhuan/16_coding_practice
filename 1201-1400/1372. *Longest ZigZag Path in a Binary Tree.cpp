f4797b1c64308030d11802d586000217988c84b7
ndhuanhuan <ndhuanhuan@gmail.com>
2020-06-04 20:34:02 -0500

    Update 1372. *Longest ZigZag Path in a Binary Tree.cpp

--- 1201-1400/1372. *Longest ZigZag Path in a Binary Tree.cpp
+++ 1201-1400/1372. *Longest ZigZag Path in a Binary Tree.cpp
@@ -9,8 +9,8 @@ public:
   // Returns {left, right, max}
   tuple<int, int, int> ZigZag(TreeNode* root) {
     if (!root) return {-1, -1, -1};
-    auto [ll, lr, lm] = ZigZag(root->left);
-    auto [rl, rr, rm] = ZigZag(root->right);
+    auto [ll, lr, lm] = ZigZag(root->left);  // lr means, currently taking left, deeper node taking right(since index is 1)
+    auto [rl, rr, rm] = ZigZag(root->right); // rl means currently taking right, deeper node taking left
     int l = lr + 1;
     int r = rl + 1;
     return {l, r, max({l, r, lm, rm})};
