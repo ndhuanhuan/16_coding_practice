03392e1ed4c8d5dfcbde878cd7d62cf1037fd4db
ndhuanhuan <ndhuanhuan@gmail.com>
2020-06-19 12:29:06 -0500

    Update linked_list_templates.cpp

--- code_templates/linked_list_templates.cpp
+++ code_templates/linked_list_templates.cpp
@@ -1,1 +1,12 @@
-
+// check if there's a cycle
+bool hasCycle(ListNode *head) {
+    auto slow = head;
+    auto fast = head;
+    while (fast) {
+      if (!fast->next) return false;
+      fast = fast->next->next;
+      slow = slow->next;
+      if (fast == slow) return true;
+    }
+    return false;
+  }
