0bd2581b0a74aedb275f73b88a558b190b296237
ndhuanhuan <ndhuanhuan@gmail.com>
2020-06-19 13:06:45 -0500

    Update linked_list_templates.cpp

--- code_templates/linked_list_templates.cpp
+++ code_templates/linked_list_templates.cpp
@@ -10,3 +10,24 @@ bool hasCycle(ListNode *head) {
     }
     return false;
   }
+
+
+
+
+
+
+// two pointer template
+ListNode* slow = head;
+ListNode* fast = head;
+/**
+ * Change this condition to fit specific problem.
+ * Attention: remember to avoid null-pointer error
+ **/
+while (slow && fast && fast->next) {
+    slow = slow->next;          // move slow pointer one step each time
+    fast = fast->next->next;    // move fast pointer two steps each time
+    if (slow == fast) {         // change this condition to fit specific problem
+        return true;
+    }
+}
+return false;   // change return value to fit specific problem
